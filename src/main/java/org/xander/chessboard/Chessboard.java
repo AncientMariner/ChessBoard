81854f9cd8ebdba98f05f6543285038312247b79
AncientMariner <xander.lysak@gmail.com>
2014-10-06 19:27:45 +0300

    draw an empty board with specific dimension

--- src/main/java/org/xander/chessboard/Chessboard.java
+++ src/main/java/org/xander/chessboard/Chessboard.java
@@ -3,11 +3,25 @@ package org.xander.chessboard;
 public class Chessboard {
     private int dimension;
 
+    public int getDimension() {
+        return dimension;
+    }
+
     public void setDimension(int dimension) {
         this.dimension = dimension;
     }
 
-    public int getDimension() {
-        return dimension;
+    public String drawABoard() {
+        int xDimension = dimension;
+        int yDimension = dimension;
+
+        StringBuilder chessBoard = new StringBuilder();
+        for (int y = 0; y < yDimension; y++) {
+            for (int x = 0; x < xDimension; x++) {
+                chessBoard.append(".");
+            }
+            chessBoard.append("\n");
+        }
+        return chessBoard.toString();
     }
 }
--- src/test/java/org/xander/chessboard/ChessboardTest.java
+++ src/test/java/org/xander/chessboard/ChessboardTest.java
@@ -1,16 +1,41 @@
 package org.xander.chessboard;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class ChessboardTest {
+    Chessboard chessboard;
+
+    @Before
+    public void setUp() {
+        chessboard = new Chessboard();
+    }
+
     @Test
-    public void chessBoardTest() {
-        Chessboard chessboard = new Chessboard();
+    public void chessBoardBasicTest() {
+        assertNotNull(chessboard);
+        assertTrue(chessboard.getDimension() == 0);
+
         int dimension = 5;
         chessboard.setDimension(dimension);
 
         assertEquals(dimension, chessboard.getDimension());
+    }
+
+    @Test
+    public void drawAnEmptyBoardTest() {
+        int dimension = 5;
+        chessboard.setDimension(dimension);
+
+        String emptyBoard = chessboard.drawABoard();
+        assertEquals(".....\n" +
+                     ".....\n" +
+                     ".....\n" +
+                     ".....\n" +
+                     ".....\n", emptyBoard);
     }
 }
