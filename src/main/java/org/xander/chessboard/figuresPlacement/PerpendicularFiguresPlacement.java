245830645d9f4d27ef8c9b8232357430d961e135
AncientMariner <xander.lysak@gmail.com>
2016-10-27 16:44:58 +0300

    structure change

--- src/main/java/org/xander/chessboard/figuresPlacement/PerpendicularFiguresPlacement.java
+++ src/main/java/org/xander/chessboard/figuresPlacement/PerpendicularFiguresPlacement.java
@@ -1,11 +1,4 @@
 package org.xander.chessboard.figuresPlacement;
 
 public abstract class PerpendicularFiguresPlacement extends PerpendicularAndDiagonalFiguresPlacement {
-    public void placeHorizontally(char[] boardElements, int position, int dimension) {
-        super.placeHorizontally(boardElements, position, dimension);
-    }
-
-    public void placeVertically(char[] boardElements, int position, int dimension) {
-        super.placeVertically(boardElements, position, dimension);
-    }
 }
